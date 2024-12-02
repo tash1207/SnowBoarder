2f6d2600ef9c752fb0ec5969f1b83c8cc862d997
Tasha Hankewych <thankewych@gmail.com>
2024-12-01 23:21:08 -0600

    Set up a finish line and a crash detector script to restart the scene if the boarder reaches the finish line or if their head hits the snow

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Ground")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
